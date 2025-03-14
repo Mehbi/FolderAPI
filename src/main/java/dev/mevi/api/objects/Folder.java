ee41584e63c5ba3adcbacf86f6ae555a79fd3e64
Mehbi <briambusiness@gmail.com>
2025-03-14 12:13:47 -0300

    New things were added and bugs fixed

--- src/main/java/dev/mevi/api/objects/Folder.java
+++ src/main/java/dev/mevi/api/objects/Folder.java
@@ -8,10 +8,10 @@ import java.nio.file.FileAlreadyExistsException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Folder {
     private final String directory;
-
 
     private Folder(String directory) {
         this.directory = directory;
@@ -21,13 +21,13 @@ public class Folder {
         return new Folder(directory);
     }
     public File newFile(String name, String extension) throws IOException {
-        File file = new File(directory, name + "." + extension);
+        File file = new File(this.directory, name + "." + extension);
         if (contains(file)) throw new FileAlreadyExistsException(file.getName());
         if (file.createNewFile()) System.out.println(file.getName() + " has been created");
         return file;
     }
-    public Folder newDirectory(String directoryName) throws FileAlreadyExistsException {
-        File file = new File(directory, directoryName);
+    public Folder newDirectory(String directoryName) {
+        File file = new File(this.directory, directoryName);
         if (contains(file.getName())) {
             System.out.println("This directory already exists");
             return null;
@@ -35,7 +35,7 @@ public class Folder {
 
         file.mkdirs();
         System.out.println(file.getName() + " has been created");
-        return Folder.from(directoryName);
+        return Folder.from(this.directory + "/" + directoryName);
     }
 
     public boolean contains(String fileName, String extension) {
@@ -54,11 +54,19 @@ public class Folder {
     public List<File> filteredByExtension(String extension) {
         return files().stream().filter(file -> Files.getFileExtension(file.getName()).equals(extension)).toList();
     }
+    public void delete(String name, String extension) {
+        Optional<File> fileFiltered = filteredByName(name).stream().filter(file -> Files.getFileExtension(name).equals(extension)).findFirst();
+        if (fileFiltered.isEmpty()) {
+            System.out.println(name + "." + extension + "could not be deleted, because it does not exists");
+            return;
+        }
+        fileFiltered.get().delete();
+    }
     private List<File> files() {
-        File file = new File(directory);
+        File file = new File(this.directory);
         return file.listFiles() != null ? Arrays.stream(file.listFiles()).toList() : new ArrayList<>();
     }
     public String directory() {
-        return directory;
+        return this.directory;
     }
 }
